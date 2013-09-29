eec71d63128c9b9fd1a682df2ab830699cbf8e2a
Seetha Annamraju <seethaa@cmu.edu>
2013-09-29 10:00:09 -0700

    removing dexfileys and still working with profileactiity

--- src/com/yahoo/yrak/ProfileActivity.java
+++ src/com/yahoo/yrak/ProfileActivity.java
@@ -2,23 +2,26 @@ package com.yahoo.yrak;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity{
 
 	Button change_loc_button;
 	Button yahoo_login;
+	TextView curr_loc;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayShowTitleEnabled(true);
 		setContentView(R.layout.activity_profile);
+		
+		curr_loc = (TextView) findViewById(R.id.textView1);
+		setCurrentLocation();
 		
 		change_loc_button = (Button) findViewById(R.id.change_location);
 		change_loc_button.setOnClickListener(change_loc_listener);
@@ -28,6 +31,11 @@ public class ProfileActivity extends Activity{
 		
 	}
 	
+	private void setCurrentLocation() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	private OnClickListener change_loc_listener = new OnClickListener(){
 		@Override
 		public void onClick(View v) {
