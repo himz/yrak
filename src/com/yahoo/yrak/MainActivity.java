406a7bd96fed245a2aa2aaa09411720546e455aa
Seetha Annamraju <seethaa@cmu.edu>
2013-09-29 02:19:02 -0700

    added gps calculation

--- src/com/yahoo/yrak/MainActivity.java
+++ src/com/yahoo/yrak/MainActivity.java
@@ -4,7 +4,11 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,6 +28,13 @@ public class MainActivity extends Activity {
 	private RAKItem raki;
 	private TextView random_rak;
 	private RAKItem curr_RAK;
+	
+	static double s_lat = 0.0;
+	static double s_long = 0.0;
+
+	//variables to get current location
+	LocationManager LC;
+	private String provider; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +56,48 @@ public class MainActivity extends Activity {
 
 		new_random_rak = (Button) findViewById(R.id.newButton);
 		new_random_rak.setOnClickListener(new_rak_listener);
-		
+
 		random_rak = (TextView) findViewById(R.id.textView1);
+
+		//get location details:
+		LC =(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		Criteria criteria=new Criteria();
+		provider=LC.getBestProvider(criteria, false);
+		Location loc=LC.getLastKnownLocation(provider);
+		if (loc!=null){
+			onLocationChanged(loc);
+		}
+		else{
+			Log.d("LOCATION: ", "No Provider");
+		}
+
+
+	}
+
+
+	public void onLocationChanged(Location location) {
+	   double lat=(double) (location.getLatitude());
+	   double lon=(double) (location.getLongitude());
+	   
+	   s_lat = lat;
+	   s_long = lon;
+
+	}
+
+	public void onProviderDisabled(String provider) {
+		Log.d("LOCATION: ", "provider disabled");
+
+	}
+
+	public void onProviderEnabled(String provider) {
+		Log.d("LOCATION: ", "provider enabled");
 		
+	}
 
-
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private OnClickListener done_listener = new OnClickListener(){
@@ -57,17 +105,17 @@ public class MainActivity extends Activity {
 		public void onClick(View v) {
 			//Do Something
 
-//			ParseObject rakObject = new ParseObject("RAKObject");
-//
-//			String s = "testing";
-//			rakObject.put("todo", s);
-//			rakObject.put("duration", s);
-//			rakObject.put("lat_location", s);
-//			rakObject.put("long_location", s);
-//			rakObject.put("tag", s);
-//			rakObject.put("msg", s);
-//			
-//			rakObject.saveInBackground();
+			//			ParseObject rakObject = new ParseObject("RAKObject");
+			//
+			//			String s = "testing";
+			//			rakObject.put("todo", s);
+			//			rakObject.put("duration", s);
+			//			rakObject.put("lat_location", s);
+			//			rakObject.put("long_location", s);
+			//			rakObject.put("tag", s);
+			//			rakObject.put("msg", s);
+			//			
+			//			rakObject.saveInBackground();
 			String msg = curr_RAK.getmMessage();
 			Context context = getApplicationContext();
 			CharSequence text = msg;
@@ -75,7 +123,7 @@ public class MainActivity extends Activity {
 
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
-			
+
 		}
 	};
 
@@ -86,7 +134,7 @@ public class MainActivity extends Activity {
 
 			/*ParseQuery<ParseObject> query = ParseQuery.getQuery("RAKObject");
 			 query.getInBackground("OQowgPEaWt", new GetCallback<ParseObject>() {
-			 
+
 
 					@Override
 					public void done(ParseObject arg0,
@@ -97,26 +145,28 @@ public class MainActivity extends Activity {
 							toast.show();
 						}
 						else{
-							
+
 						}
 					}
 			});*/
-			
+
+			 
+			System.out.println("LAT: "+s_lat + "LONG: "+ s_long);
 			curr_RAK =  DBHelper.getRandomRAK();
 			if (curr_RAK!=null){
 				String rak = curr_RAK.getmTodo();
 				System.out.println("rak item: " +rak);
 				random_rak.setText(rak);
 			}
-			
-			
-			
+
+
+
 		}
 	};
- 
-  
- 
-	
+
+
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
